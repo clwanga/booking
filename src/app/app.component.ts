2cbf70076fced5f8db0875acd715435f58d2e4bb
Charles Lwanga Maungila <cmaungila1095@gmail.com>
2024-11-01 19:41:15 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './Layout/navbar/navbar.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'booking';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
